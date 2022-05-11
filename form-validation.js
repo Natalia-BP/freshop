b9e64cd317c388298e8ac97cf1d5cf2e4d224afc
Natalia-BP <nataliabarqueroperez@gmail.com>
2022-05-10 21:02:53 -0600

    improvements to contact lg layout and start of validation functionality

---
+++ form-validation.js
@@ -0,0 +1,9 @@
+let contactForm = document.getElementById('pdrio-contact-main__form');
+
+contactForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    let contactName = document.getElementById('inputName');
+    let contactEmail = document.getElementById('inputEmail');
+    let contactNumber = document.getElementById('inputNumber');
+    let contactSubject = document.getElementById('inputSubject');
+})
