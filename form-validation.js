34a9c97d9405c5170746b0d721db376f7ed67433
Natalia-BP <nataliabarqueroperez@gmail.com>
2024-01-26 16:05:50 -0600

    updates to jQuery and advances in form validation

--- form-validation.js
+++ form-validation.js
@@ -3,10 +3,10 @@ let contactForm = document.getElementById('pdrio-contact-main__form');
 contactForm.addEventListener('submit', function (e) {
     e.preventDefault();
     // Inputs
-    let contactName = document.getElementById('inputName');
-    let contactEmail = document.getElementById('inputEmail');
-    let contactNumber = document.getElementById('inputNumber');
-    let contactSubject = document.getElementById('inputSubject');
+    let contactName = $('#inputName');
+    let contactEmail = $('#inputEmail');
+    let contactNumber = $('#inputNumber');
+    let contactSubject = $('#inputSubject');
 
     // RegExp
     const lettersAndSpaces = /^[A-Za-z\s]*$/;
@@ -14,29 +14,46 @@ contactForm.addEventListener('submit', function (e) {
     const numbersAndSpaces = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
     
     // Name
-    let nameValidation = lettersAndSpaces.test(contactName.value);
+    let nameValue = contactName.value;
+    let nameValidation = lettersAndSpaces.test(nameValue);
     if (!nameValidation) {
-        contactName.classList.add("--invalid");
+        contactName.addClass("is-invalid");
     } else {
-        contactName.classList.remove("--invalid");
-        contactName.classList.add("--valid");
+        contactName.removeClass("is-invalid");
+        contactName.addClass("is-valid");
     }
 
     // Email
-    let emailValidation = email.test(contactEmail.value);
-    if (!emailValidation) {
-        contactEmail.classList.add("--invalid");
-    } else {
-        contactEmail.classList.remove("--invalid");
-        contactEmail.classList.add("--valid");
+    let emailValue = contactEmail.value;
+    let emailValidation = email.test(emailValue);
+    let emptyEmailInput = $(".invalid-input.--empty");
+    let wrongEmailInput = $(".wrong-input.--empty");
+    // If it's empty
+    if (emailValue.length == 0) {
+        emptyEmailInput.removeClass("d-none");
+        contactEmail.addClass("is-invalid");
+        console.log('Aqui estoy');
+        console.log(emptyEmailInput);
+    // If it's wrong
+    }  else if (!emailValidation) {
+        wrongEmailInput.removeClass("d-none");
+        contactEmail.addClass("is-invalid");
+        console.log('O Aqui estoy')
+    } 
+    // If it's OK
+    else {
+        emptyEmailInput.addClass("d-none");
+        wrongEmailInput.addClass("d-none");
+        contactEmail.removeClass("is-invalid");
+        contactEmail.addClass("is-valid");
     }
     
     // Number
-    let numberValidation = email.test(contactNumber.value);
+    let numberValidation = numbersAndSpaces.test(contactNumber.value);
     if (!numberValidation) {
-        contactNumber.classList.add("--invalid");
+        contactNumber.addClass("is-invalid");
     } else {
-        contactNumber.classList.remove("--invalid");
-        contactNumber.classList.add("--valid");
+        contactNumber.removeClass("is-invalid");
+        contactNumber.addClass("is-valid");
     }
-})
+});
